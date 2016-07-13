d08b93cc67abcfc90d6fe63f62eb07ce184c0228
Aaron Lim <Alim@stc.syrres.com>
2016-07-13 17:48:32 -0400

    started working on compare.js

--- compare.js
+++ compare.js
@@ -1,1 +1,36 @@
-// PUT CODE HERE
+
+function main() {
+	
+}
+
+function score_toggle() {
+	var displaying_time = document.getElementById("minutes") != null;
+	var time_button_checked = document.getElementById("time").checked == true;
+
+	if (displaying_time && !time_button_checked) {
+		// Switch to displaying the distance input
+
+	} else if (!displaying_time && time_button_checked) {
+		// Switch to displaying the time input
+
+	}
+	return false;
+}
+
+$(document).ready(function() {
+
+	document.getElementById("time").addEventListener("click", score_toggle);
+	document.getElementById("distance").addEventListener("click", score_toggle);
+
+	var data = document.getElementsByName("data");
+	for(i=0; i<data.length; i++) {
+		data[i].addEventListener("keypress", function(e) {
+			var key = e.which || e.keyCode;
+			if (key === 13) {
+				return main();
+			} else {
+				return false;
+			}
+		});
+	}
+});
