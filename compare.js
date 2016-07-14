a5dd7b5aed5eec7e9d0536c4101deb2317670332
Aaron Lim <aaronzlim@gmail.com>
2016-07-13 21:17:53 -0400

    added reset and help to WA Compare.

--- compare.js
+++ compare.js
@@ -1,6 +1,19 @@
 
-function main() {
-	
+// I split these into seperate functions in order to use the
+// setTimeout() method to add a pause between fadIn and fadeOut
+function switch_to_time() {
+		document.getElementById("score1").innerHTML = '<label for="time1">Time 1: </label>' +
+					'<input type="number" id="minutes" min="0" max="120" name="data" placeholder="min" align="left" />' +
+					'<font size="4"> : </font>' +
+					'<input type="number" id="seconds" min="0" max="59.99" name="data" placeholder="sec" align="left" />';
+
+}
+
+function switch_to_distance() {
+		document.getElementById("score1").innerHTML = "<label for='distance1'>Distance 1 (meters): </label>" +
+												"<input type='number' id='distance1' name='data' min='0'" +
+												" placeholder='Dist' align='left' />";
+
 }
 
 function score_toggle() {
@@ -9,12 +22,32 @@ function score_toggle() {
 
 	if (displaying_time && !time_button_checked) {
 		// Switch to displaying the distance input
-
+		$("#score1").fadeOut("fast");
+		setTimeout(switch_to_distance, 150);
+		$("#score1").fadeIn("fast");
 	} else if (!displaying_time && time_button_checked) {
 		// Switch to displaying the time input
-
+		$("#score1").fadeOut("fast");
+		setTimeout(switch_to_time, 150);
+		$("#score1").fadeIn("fast");
 	}
 	return false;
+}
+
+function reset() {
+	document.getElementById("weight1").value = "";
+	if (document.getElementById("minutes") != null) {
+		document.getElementById("minutes").value = "";
+		document.getElementById("seconds").value = "";
+	} else {
+		document.getElementById("distance1").value = "";
+	}
+	document.getElementById("weight2").value = "";
+	document.getElementById("results").innerHTML = "";
+}
+
+function compare() {
+	return preventDefault();
 }
 
 $(document).ready(function() {
@@ -33,4 +66,14 @@ $(document).ready(function() {
 			}
 		});
 	}
+
+	document.getElementById("reset_button").addEventListener("click", reset);
+
+	document.getElementById("help_button").addEventListener("click", function() {
+		document.getElementById("results").innerHTML = "HELP:<br/>" + 
+													"Give the weight and raw score of Rower1.<br/>" +
+													"Then give the weight of Rower2.<br/>" +
+													"This program will output the raw score needed for " +
+													"Rower2 to beat Rower1 weight adjusted.";
+	});
 });
