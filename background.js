9162f21837bcbcaa80db0602df92aebb6bfaaab0
Aaron Lim <aaronzlim@gmail.com>
2016-07-17 22:35:47 -0400

    Added ability to calculate times and distances from splits.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('window.html', {
 		id: 'main',
-		bounds: {width: 515, height: 400},
+		bounds: {width: 550, height: 400},
 		resizable: false,
 		frame: {
 			type: "chrome",
